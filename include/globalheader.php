7c27156305478fc7ba676ee69d2dfe1ca15036ff
Taran221104 <146746579+Taran221104@users.noreply.github.com>
2023-12-11 11:27:07 -0500

    Add files via upload

---
+++ include/globalheader.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="../css/style.css" type="text/css">
+<style>
+  .custom-navbar{
+    background-color:#79AC78;
+  }
+</style>
+
+<nav class="navbar navbar-dark custom-navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><img src="images/logo.jpg" alt="header logo"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="display-person.php">View</a></li>
+                    <li class="nav-item"><a class="nav-link" href="signin.php">Sign In</a></li>
+                    <li class="nav-item"><a class="nav-link" href="addimage.php">ADD Images</a></li>
+                    <li class="nav-item"><a class="nav-link" href="viewimage.php">View Images</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
